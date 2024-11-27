8e907fd7009e010eff7a53e800e625ae04b9b8d7
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2024-11-27 07:54:24 -0300

    Finishing the raw project

--- src/resources/js/Pages/Pizzas/Show.jsx
+++ src/resources/js/Pages/Pizzas/Show.jsx
@@ -1,6 +1,19 @@
-import {Head} from "@inertiajs/react";
+import {Head, router} from "@inertiajs/react";
+import PizzaStatus from "@/Pages/Pizzas/Partials/PizzaStatus.jsx";
+import {useEffect} from "react";
 
 const Show = ({ pizza }) => {
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            router.reload({ only: ['pizza'] })
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
+
     return (
         <div className="max-w-3xl mx-auto py-12">
             <Head title={'Order #' + pizza.id}/>
@@ -14,7 +27,7 @@ const Show = ({ pizza }) => {
                     </g>
                 </svg>
             </div>
-
+            <PizzaStatus currentStatus={pizza.status}></PizzaStatus>
             <div className="text-center mt-4">
                 <p className="text-lg">
                     {pizza.chef} started {pizza.status.toLowerCase()} your order <span className="underline font-semibold">{pizza.last_updated}</span>
